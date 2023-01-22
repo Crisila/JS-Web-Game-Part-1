b2b3a0eb13760ecdc4e2070d104646929eae06c3
Crisila Aban <Crisila@MacBook-Pro-Alisirc.local>
2023-01-22 11:17:18 -0800

    add pine Tree

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+
+
+// Duplicate the code in index.js and edit it to add a pine tree to the screen. The image is located at assets/pine-tree.png and should be placed 450px to the left and 200px from the bottom. When you are finished, the browser screen should look like this:
+let pineTree = document.createElement('img')
+pineTree.src = '/Users/Crisila/JS-Web-Game-Part-1/assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
